28e814c88fb6c55786fd9581730dedd4d8222eb5
gatoko <g.millaquiensuarez@gmail.com>
2018-03-01 11:25:02 -0300

    Ejercicio Finalizado.

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //8) Escribir un método que acepte un int y te devuelva el factorial de dicho número.
+//Nota: El factorial de 3 es 3*2*1, el factorial de 5 es 5*4*3*2*1, etc.
 
 namespace Factorial
 {
@@ -12,6 +13,30 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
+
+            while (true)
+            {
+
+                try
+                {
+                    int numero, resultado = 1;
+
+                    Console.WriteLine("Ingrese un número para calcular su factorial: ");                   
+                    numero = int.Parse(Console.ReadLine());
+                    for (int i = 1; i <= numero; i++)
+                    {
+                        resultado = resultado * i;
+                    }
+                    Console.WriteLine("El factorial de " + numero + " es:" + resultado);
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Escribe algún valor válido");
+                }
+
+            }
+
         }
     }
 }
