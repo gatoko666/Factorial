139035349fa5c74ba8ed095b6f5ed3c1c74140ba
gatoko <g.millaquiensuarez@gmail.com>
2018-03-01 11:09:12 -0300

    Inicio.

---
+++ Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//8) Escribir un método que acepte un int y te devuelva el factorial de dicho número.
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
